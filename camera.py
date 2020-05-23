6fc12dbba804d8ab1c7664f31070b33ee1d9db61
Milan <milan@mbpofhereygers.speedport_w_724v_09011603_06_006>
2020-05-23 15:36:29 +0200

    first commit4

---
+++ camera.py
@@ -0,0 +1,18 @@
+from picamera import PiCamera
+import datetime
+from time import sleep
+t = datetime.datetime.now()
+
+
+camera = PiCamera()
+camera.start_preview()
+sleep(5)
+p = '/home/pi/picam/{}.jpg'.format(t)
+p = p.replace(" ","_")
+
+
+camera.capture(p)
+#camera.annotate_text = time.time()
+#camera.annotate_text_size = 50 # (values 6 to 160, default is 32)
+#camera.annotate_text = dt.datetime.now().strftime('%A %d %b %Y %H:%M')
+camera.stop_preview()
---
+++ video.py
@@ -0,0 +1,15 @@
+from picamera import PiCamera
+from time import sleep
+import datetime
+
+
+t = datetime.datetime.now()
+p = '/home/pi/picam/{}.h264'.format(t)
+
+p = p.replace(" ","_")
+camera = PiCamera()
+camera.start_preview()
+camera.start_recording(p)
+sleep(10)
+camera.stop_recording()
+camera.stop_preview()
